0d610314f56cdf13f0434d9dea9a746a7ee4ee04
Cristiann-Paredes <paredes30@hotmail.es>
2023-06-21 13:00:24 -0500

    sets y gets

--- src/Leon.java
+++ src/Leon.java
@@ -1,2 +1,34 @@
-public class Leon {
+public class Leon extends felinos {
+    private String pelaje;
+    private String manada;
+    private String camuflaje;
+
+    public Leon(String tamaño, String edad, String garras, String bigotes,
+                String color, String pelaje, String manada, String camuflaje) {
+        super(tamaño, edad, garras, bigotes, color);
+
+        this.pelaje = pelaje;
+        this.manada = manada;
+        this.camuflaje = camuflaje;}
+
+    public String getPelaje() {return pelaje;}
+    public void setPelaje(String pelaje) {this.pelaje = pelaje;}
+    public String getManada() {return manada;}
+    public void setManada(String manada) {this.manada = manada;}
+    public String getCamuflaje() {return camuflaje;}
+    public void setCamuflaje(String camuflaje) {this.camuflaje = camuflaje;}
+    //metodos
+    public void rugir(){
+        System.out.println("El rugido del león es uno de los más temidos de la sabana ");}
+    public void asechar(){
+        System.out.println("Tienen una manera muy particular y sincronizada de asechar a otros animales ");}
+    public void proteger(){
+        System.out.println("Leones se protegen con su tamaño intimidante, dientes afilados y garras afiladas.");}
+
+
+
+
+
+
+
 }
--- src/felinos.java
+++ src/felinos.java
@@ -1,2 +1,44 @@
 public class felinos {
+
+    private String tamaño;
+    private String edad;
+    private String garras;
+    private  String bigotes;
+    private String color;
+
+
+    public felinos(){
+    }
+
+    public felinos(String tamaño, String edad, String garras, String bigotes, String color) {
+        this.tamaño = tamaño;
+        this.edad = edad;
+        this.garras = garras;
+        this.bigotes = bigotes;
+        this.color = color;
+    }
+
+    public String getTamaño() {return tamaño;}
+    public void setTamaño(String tamaño) {this.tamaño = tamaño;}
+    public String getEdad() {return edad;}
+    public void setEdad(String edad) {this.edad = edad;}
+    public String getGarras() {return garras;}
+    public void setGarras(String garras) {this.garras = garras;}
+    public String getBigotes() {return bigotes;}
+    public void setBigotes(String bigotes) {this.bigotes = bigotes;}
+    public String getColor() {return color;}
+    public void setColor(String color) {this.color = color;}
+
+    //metodos
+    public void Comer(){
+        System.out.println("Los felinos se alimentan principalmente de carne");
+    }
+    public void dormir(){
+        System.out.println("Los felinos duermen entre 12 y 16 horas");
+    }
+    public void trepar(){
+        System.out.println("La mayoria de felinos pueden trepar ");
+    }
+
+
 }
